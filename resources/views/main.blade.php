e9c1769f5c95a7314b0ff8a772b059b7824fe331
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-01-31 11:31:05 +0700

    Menambahkan tampilan card welcome pada view main

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,1 +1,50 @@
-<x-layout breadcrumbs="main"></x-layout>
+<x-layout breadcrumbs="main">
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="card overflow-hidden">
+                <div class="bg-primary bg-soft-primary">
+                    <div class="row">
+                        <div class="col-7">
+                            <div class="text-primary p-3">
+                                <h5 class="text-primary">Welcome Back !</h5>
+                                <p>Skote Dashboard</p>
+                            </div>
+                        </div>
+                        <div class="col-5 align-self-end">
+                            <img src="{!! asset('src/images/profile-img.png') !!}" alt="" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body pt-0">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="avatar-md profile-user-wid mb-4">
+                                <img src="{!! asset('src/images/user-img.jpg') !!}" alt="" class="img-thumbnail rounded-circle">
+                            </div>
+                            <h5 class="font-size-15 text-truncate">Henry Price</h5>
+                            <p class="text-muted mb-0 text-truncate">UI/UX Designer</p>
+                        </div>
+                        {{--<div class="col-sm-8">
+                            <div class="pt-4">
+
+                                <div class="row">
+                                    <div class="col-6">
+                                        <h5 class="font-size-15">125</h5>
+                                        <p class="text-muted mb-0">Projects</p>
+                                    </div>
+                                    <div class="col-6">
+                                        <h5 class="font-size-15">$1245</h5>
+                                        <p class="text-muted mb-0">Revenue</p>
+                                    </div>
+                                </div>
+                                <div class="mt-4">
+                                    <a href="javascript: void(0);" class="btn btn-primary waves-effect waves-light btn-sm">View Profile <i class="mdi mdi-arrow-right ms-1"></i></a>
+                                </div>
+                            </div>
+                        </div>--}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
