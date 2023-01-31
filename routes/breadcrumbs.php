9670832a41c74de6fa72d37a40222ba5fb0336a5
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-01-31 11:24:36 +0700

    Integrasi package diglactic/laravel-breadcrumbs ke dalam project

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,21 @@
+<?php
+
+use Diglactic\Breadcrumbs\Breadcrumbs;
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+
+Breadcrumbs::for('main',function (BreadcrumbTrail $trail){
+    $trail->push('Home',route('main'));
+});
+
+//region Master RM
+Breadcrumbs::for('master-rm',function (BreadcrumbTrail $trail){
+    $trail->parent('main');
+    $trail->push('Master RM');
+});
+//region Fabric
+/*Breadcrumbs::for('fabric',function (BreadcrumbTrail $trail){
+    $trail->parent('master-rm');
+    $trail->push('Jenis Fabric',route('fabric.index'));
+});*/
+//endregion
+//endregion
