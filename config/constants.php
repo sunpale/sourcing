6f4e75c78631587978120e924825145215970034
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-02-03 09:23:24 +0700

    Menambahkan file constanst.php untuk menampung variable konstanta yang digunakan di program

---
+++ config/constants.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'CONFIRM_TITLE_EDIT'    => 'Apakah anda yakin akan mengedit data ini ?',
+    'CONFIRM_TITLE_DELETE'  =>'Apakah anda yakin akan menghapus data ini ?',
+    'CONFIRM_TITLE_SAVE'    => 'Apakah anda yakin akan menyimpan data ini ?',
+    'WARNING_MESSAGE'       =>'Peringatan! Tindakan ini tidak bisa dibatalkan!',
+    'SUCCESS_SAVE'          => 'Data telah berhasil disimpan',
+    'SUCCESS_DELETE'        => 'Data telah berhasil dihapus',
+    'SUCCESS_UPDATE'        => 'Data telah berhasil diubah',
+    'FAILED_SAVE'           => 'Data gagal disimpan',
+    'FAILED_UPDATE'         => 'Data gagal diubah',
+    'FAILED_DELETE'         => 'Data gagal dihapus',
+    'TRANSAKSI_OPENING'     => 'OP',
+    'TRANSAKSI_TERIMA_BARANG_VENDOR'    => 'TBV',
+    'TRANSAKSI_KIRIM_BARANG'            => 'SJ',
+    'TRANSAKSI_REALISASI_OP'            => 'REAL',
+    'KODE_TRANSAKSI_OPENING'                => '10',
+    'KODE_TRANSAKSI_TERIMA_BARANG_VENDOR'   => '20',
+    'KODE_TRANSAKSI_KIRIM_BARANG'           => '30',
+    'KODE_TRANSAKSI_REALISASI_OP'           => '11',
+    'PAGE_ACCESS_NOT_ALLOWED'   => 'Anda tidak memiliki izin yang memadai untuk mengakses halaman ini',
+    'FITUR_ACCESS_NOT_ALLOWED'  => 'Anda tidak memiliki izin yang memadai untuk mengakses fitur ini'
+];
