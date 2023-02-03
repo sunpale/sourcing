d0d561612e104737df8fd2a0844565472da56c1e
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-02-03 18:32:21 +0700

    Membuang field yang tidak diperluan di attribute fillable

--- app/Models/master_rm/Fabric.php
+++ app/Models/master_rm/Fabric.php
@@ -10,5 +10,5 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Fabric extends Model
 {
     use UserInput,CustomSoftDeleteTrait,SoftDeletes {CustomSoftDeleteTrait::runSoftDelete insteadof SoftDeletes;}
-    protected $fillable = ['kode','description','created_by','updated_by','deleted_by','created_at','updated_at','deleted_at'];
+    protected $fillable = ['kode','description'];
 }
