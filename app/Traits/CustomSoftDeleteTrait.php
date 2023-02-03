f619a4f1526a48314e9c3b728f38605cb607b30d
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-02-03 09:30:08 +0700

    Membuat CustomSoftDeleteTrait
    Berfungsi untuk menggantikan SoftDelete bawaan laravel dengan menambahkan pengisian kolom deleted_by

---
+++ app/Traits/CustomSoftDeleteTrait.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+trait CustomSoftDeleteTrait {
+    protected function runSoftDelete(): void
+    {
+        $keyName = $this->getKeyName();
+        $keyValue = $this->getKey();
+        $tableName = $this->getTable();
+
+        if (key_exists('deleted_by', $this->attributes)) {
+            $updateColumns = [$this->getDeletedAtColumn() => $this->freshTimestamp(), 'deleted_by' => Auth::user()->id];
+        } else {
+            $updateColumns = [$this->getDeletedAtColumn() => $this->freshTimestamp()];
+        }
+
+        // Do not use the model to do the update, as it will also update the updated_at column.
+        DB::table($tableName)
+            ->where($keyName, '=', $keyValue)
+            ->update($updateColumns);
+    }
+}
