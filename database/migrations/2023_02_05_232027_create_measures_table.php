0db474aaf7779ce7980eeda45eeed4f13f779a81
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-02-06 03:34:35 +0700

    Membuat migrasi table untuk table unit of measure

---
+++ database/migrations/2023_02_05_232027_create_measures_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Schemas\Grammars\CustomSqlServerGrammar;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        if (env('DB_CONNECTION')==='sqlsrv'){
+            DB::connection()->setSchemaGrammar(new CustomSqlServerGrammar());
+        }
+        Schema::create('measures', function (Blueprint $table) {
+            $table->tinyIncrements('id')->unique();
+            $table->string('kode',3)->unique();
+            $table->string('measure_name',25)->index();
+            $table->smallInteger('created_by')->index();
+            $table->smallInteger('updated_by')->index();
+            $table->smallInteger('deleted_by')->nullable()->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('measures');
+    }
+};
