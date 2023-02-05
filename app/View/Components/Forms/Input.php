c59e84d636722f4fbb463b6b7a71d6e9bcb56199
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-02-05 16:41:51 +0700

    Penyederhaan blade komponen form input dan membuatnya lebih fleksibel
    Fleksibel dalam menambah property dan class

--- app/View/Components/Forms/Input.php
+++ app/View/Components/Forms/Input.php
@@ -7,42 +7,18 @@ use Illuminate\View\Component;
 
 class Input extends Component
 {
-    public string $label;
     public string $id;
-    public string $type;
+    public string $label;
     public string $name;
     public string $value;
-    public string $placeholder;
-    public bool $isRequired;
     public string $marginBottom;
-    public string $formInput;
-    public bool $readOnly;
-    public string $maxLength;
-
-    public function __construct(string|null $formInput= null, string|null $label = null, string|null $id = null, string|null $type = null, string|null $name = null, string|null $value = null, bool|null $isRequired = null, string|null $placeholder = null, string|null $marginBottom = null,bool|null $readOnly=null,int|null $maxLength = null)
+    public function __construct(string $id = null,string $name = '',string $value = '', string $marginBottom='mb-2',string $label = '')
     {
-        $marginBottom ??= 'mb-2';
-        $placeholder ??= '';
-        $isRequired ??= false;
-        $value ??= '';
-        $name ??= '';
-        $type ??= 'text';
-        $id ??= '';
-        $label ??= '';
-        $formInput ??= 'input';
-        $readOnly??=false;
-        $maxLength ??= '';
-        $this->formInput = $formInput;
-        $this->label = $label;
-        $this->id   = $id;
-        $this->type = $type;
+        $this->id = $id;
         $this->name = $name;
         $this->value = $value;
-        $this->isRequired = $isRequired;
-        $this->placeholder = $placeholder==='' ? $label:$placeholder;
         $this->marginBottom = $marginBottom;
-        $this->readOnly = $readOnly;
-        $this->maxLength = $maxLength;
+        $this->label = $label;
     }
 
     public function render(): View
--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -1,14 +1,7 @@
 <div class="{{$marginBottom}}">
     <label class="form-label" for="{{$id}}">{{$label}}</label>
-    @if($formInput==='input')
-        <input type="{{$type}}" id="{{$id}}" class="form-control @error($name) is-invalid @enderror " name="{{$name}}" value="{{old($name)??$value}}" placeholder="{{$placeholder}}" {{$isRequired ? 'required' : ''}} {{$readOnly ? 'readonly':''}} maxlength="{{$maxLength}}">
-        @error($name)
-        <div class="invalid-feedback">{{$message}}</div>
-        @enderror
-    @else
-        <textarea class="form-control  @error($name) is-invalid @enderror" id="{{$id}}" name="{{$name}}" placeholder="{{$placeholder}}" {{$isRequired ? 'required':''}} rows="3">{{old($name)??$value}}</textarea>
-        @error($name)
-        <div class="invalid-feedback">{{$message}}</div>
-        @enderror
-    @endif
+    <input id="{{$id}}" name="{{$name}}" {{$attributes->merge(['class'=>'form-control'.($errors->has($name) ? ' is-invalid' : '')])}}  value="{{old($name)??$value}}" {{$attributes->merge(['type'=>'text'])}}>
+    @error($name)
+    <div class="invalid-feedback">{{$message}}</div>
+    @enderror
 </div>
