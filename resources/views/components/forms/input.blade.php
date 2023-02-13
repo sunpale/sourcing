9a9baf255eba7c906529999a8a98461c903bb88b
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-02-13 20:00:53 +0700

    Optimalisasi kode pada komponen form input saat menampilkan data

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -1,6 +1,6 @@
 <div class="{{$marginBottom}}">
     <label class="form-label" for="{{$id}}">{{$label}}</label>
-    <input id="{{$id}}" name="{{$name}}" {{$attributes->merge(['class'=>'form-control'.($errors->has($name) ? ' is-invalid' : '')])}}  value="{{old($name)??$value}}" {{$attributes->merge(['type'=>'text'])}}>
+    <input id="{{$id}}" name="{{$name}}" {{$attributes->merge(['class'=>'form-control'.($errors->has($name) ? ' is-invalid' : '')])}}  value="{{$errors->has($name) ? old($name):$value}}" {{$attributes->merge(['type'=>'text'])}}>
     @error($name)
     <div class="invalid-feedback">{{$message}}</div>
     @enderror
