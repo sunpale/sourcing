a61312e5271e87874a71badca2ca87bdbf3b43b8
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-02-13 20:00:03 +0700

    Membuat relasi model antara supplier dan product_group

--- app/Models/master_aks/ProductGroup.php
+++ app/Models/master_aks/ProductGroup.php
@@ -2,9 +2,11 @@
 
 namespace App\Models\master_aks;
 
+use App\Models\master_data\Supplier;
 use App\Traits\CustomSoftDelete;
 use App\Traits\UserInput;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductGroup extends Model
@@ -12,4 +14,9 @@ class ProductGroup extends Model
     use UserInput,CustomSoftDelete,SoftDeletes {CustomSoftDelete::runSoftDelete insteadof SoftDeletes;}
 
     protected $fillable = ['group','remarks'];
+
+    public function Supplier(): HasMany
+    {
+        return $this->hasMany(Supplier::class,'product_group_id','id');
+    }
 }
