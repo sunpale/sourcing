028d4fdbbc888375916663eea4fc2cb099d7a335
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-02-20 07:07:10 +0700

    Perbaikan dan optimalisasi Kode
    - Perbaikan proses saat simpan data
    - Pemisahan method pembuat kode
    - Regenerate kode saat simpan data untuk mencegah duplikasi kode
    - Menyelesaikan method show untuk menampilkan seluruh data di view main.blade
    - Menyelesaikan method destroy
    - Mengubah nama parameter method $material menjadi $raw_material

--- app/Http/Controllers/master_rm/MaterialsController.php
+++ app/Http/Controllers/master_rm/MaterialsController.php
@@ -8,10 +8,11 @@ use App\Models\master_data\Brand;
 use App\Models\master_data\Measure;
 use App\Models\master_rm\Fabric;
 use App\Models\master_rm\Komposisi;
-use App\Models\master_rm\material;
+use App\Models\master_rm\Material;
 use App\Models\master_warna\Color;
 use App\Models\master_warna\Pantone;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class MaterialsController extends Controller
 {
@@ -42,33 +43,59 @@ class MaterialsController extends Controller
     public function store(MaterialRequest $request)
     {
         $data = $request->except(['_token','number']);
-        $data['kode']=$request->number;
-        material::create($data);
+        $kodeGenerated = $request->number;
+        $prefix = substr($kodeGenerated,0,strlen($kodeGenerated)-3);
+        $data['kode']=$this->createCode($prefix);
+        Material::create($data);
         return redirect()->route('raw-material.index')->with('success',config('constants.SUCCESS_SAVE'));
     }
 
-    public function show(material $material)
+    public function show()
+    {
+        if(request()->ajax()){
+            $query = Material::select(['kode','kode_infor','fabric_id','color_id','brand_id','supplier_id','komposisi_id','item_name','item_desc','measure_id'])
+                ->with(['fabric:id,description','color:id,description','brand:id,brand','supplier:id,name','komposisi:id,komposisi','measure:id,kode,measure_name']);
+            return DataTables::eloquent($query)
+                ->addIndexColumn()->addColumn('responsive',function (){return '';})
+                ->addColumn('action',function ($row){
+                    return '<div class="dropdown d-inline-block"><button class="btn btn-soft-primary btn-sm dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="ri-equalizer-fill align-middle"></i></button><ul class="dropdown-menu dropdown-menu-end"><li><a href="#" data-bs-toggle="tooltip" data-placement="auto" title="Edit Data" class="dropdown-item" onclick=edit("'.$row->kode.'")><i class="ri-edit-fill"></i> Edit Data</a></li><li><a href="#" data-bs-toggle="tooltip" data-placement="auto" title="Hapus Data" class="dropdown-item" onclick=hapus("'.$row->kode.'")><i class="ri-close-circle-fill"></i> Hapus Data</a></li></ul></div>';
+                })->make(true);
+        }
+    }
+
+    public function edit(Material $raw_material)
     {
     }
 
-    public function edit(material $material)
+    public function update(Request $request, Material $raw_material)
     {
     }
 
-    public function update(Request $request, material $material)
+    public function destroy(Material $raw_material)
     {
+        Material::destroy($raw_material->kode);
+        return redirect()->route('raw-material.index')->with('success',config('constants.SUCCESS_DELETE'));
     }
 
-    public function destroy(material $material)
-    {
+    private function createCode(string $prefix){
+        $lastKode = Material::select('kode')->where('kode','LIKE','%'.$prefix.'%')->orderBy('created_at','desc')->withTrashed()->first();
+        if(!isset($lastKode)){
+            return $prefix.'001';
+        }
+        $number = substr($lastKode->kode,11,4);
+        $id = intval($number);
+        $id += 1;
+        $idLength = strlen($id);
+        $prefixCode='';
+        for ($i=$idLength;$i<3;$i++){
+            $prefixCode .= '0';
+        }
+        return $prefix.$prefixCode.$id;
     }
 
     public function generateCode(Request $request){
-        $prefix = $request->prefixCode;
-        $lastKode = material::select('kode')->where('kode','LIKE','%'.$prefix.'%')->orderBy('created_at','desc')->withTrashed()->first();
-        if(!isset($lastKode)){
-            $result = array('kode'=>$prefix.'001');
-        }
+        $kode = $this->createCode($request->prefixCode);
+        $result = ['kode'=>$kode];
         return response()->json($result);
     }
 }
