77d7a8559042679369c9cc650b3a8078c5817ac3
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-02-20 07:03:35 +0700

    Memperbaiki penamaan Class model Material

---
+++ app/Models/master_rm/Material.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models\master_rm;
+
+use App\Models\master_data\Brand;
+use App\Models\master_data\Measure;
+use App\Models\master_data\Supplier;
+use App\Models\master_warna\Color;
+use App\Traits\CustomSoftDelete;
+use App\Traits\UserInput;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Material extends Model
+{
+    use UserInput,CustomSoftDelete,SoftDeletes {CustomSoftDelete::runSoftDelete insteadof SoftDeletes;}
+
+    protected $primaryKey = 'kode';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $fillable = ['kode','kode_infor','fabric_id','color_id','brand_id','supplier_id','pantone_id','product_group_id','color_aks_id','komposisi_id','item_name','item_desc','gramasi','lebar','susut','finish','lead_time','moq','moq_color','ppn','measure_id','image_path','image_name'];
+
+    public function Fabric(): BelongsTo
+    {
+        return $this->belongsTo(Fabric::class);
+    }
+
+    public function Color(): BelongsTo
+    {
+        return $this->belongsTo(Color::class);
+    }
+
+    public function Brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function Supplier(): BelongsTo
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function Komposisi(): BelongsTo
+    {
+        return $this->belongsTo(Komposisi::class);
+    }
+
+    public function Measure(): BelongsTo
+    {
+        return $this->belongsTo(Measure::class);
+    }
+}
