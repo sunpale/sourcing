f760a2da0880e05f0c033fb733be9d87c7bbf4a6
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-02-21 07:12:56 +0700

    Membuat view main dan create untuk halaman master aksesoris

---
+++ resources/views/master_aks/create.blade.php
@@ -0,0 +1,3 @@
+<x-material-form :form="'AKS'" :group="$dataGroup->toArray()" :warna="$dataWarna->toArray()" :brand="$dataBrand->toArray()" :measure="$dataMeasure->toArray()" :warna-aks="$dataWarnaAks->toArray()">
+
+</x-material-form>
---
+++ resources/views/master_aks/main.blade.php
@@ -0,0 +1,137 @@
+<x-layout breadcrumbs="material" :datatable="true" :sweetalert="true" :toastify="true" :freeze-ui="true">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header align-items-center d-flex">
+                    <h4 class="card-title mb-0 flex-grow-1">Data Aksesoris</h4>
+                    <div class="flex-shrink-0">
+                        <a href="{!! route('aksesoris.create') !!}" class="btn btn-sm btn-info waves-effect waves-light">
+                            <i class="fal fa-file-plus"></i> Add new
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body">
+                    @csrf
+                    <table id="tbl-aksesoris" class="table table-sm table-bordered table-hover dt-responsive nowrap table-striped align-middle w-100 fw">
+                        <thead>
+                        <tr>
+                            <th></th>
+                            <th class="text-center">No</th>
+                            <th class="text-center">ID</th>
+                            <th class="text-center">ID Infor</th>
+                            <th class="text-center">Product Group</th>
+                            <th class="text-center">Color</th>
+                            <th class="text-center">Brand</th>
+                            <th class="text-center">Supplier</th>
+                            <th class="text-center">Item Name</th>
+                            <th class="text-center">Item Desc</th>
+                            <th class="text-center">UOM</th>
+                            <th class="text-center">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    @section('script')
+        <script>
+            function tableAksesoris(){
+                const aksesoris = $('#tbl-aksesoris').DataTable({
+                    serverSide: true,
+                    ajax: '{{route('raw-material.data',1)}}',
+                    columns: [
+                        {data: 'responsive',name:'responsive',searchable:false},
+                        {data: 'rownum',name:'rownum',searchable:false}, {data: 'kode',name: 'kode'}, {data: 'kode_infor',name: 'kode_infor'}, {data: 'fabric.description',name: 'fabric.description'},{data: 'color.description',name: 'color.description'}, {data: 'brand.brand',name: 'brand.brand'}, {data: 'supplier.name',name: 'supplier.name'},{data : 'item_name',name: 'item_name'},{data : 'item_desc',name: 'item_desc'},{data : 'komposisi.komposisi',name: 'komposisi.komposisi'},{data:'measure.kode',data:'measure.kode'},{data: 'action',name: 'action'}
+                    ],
+                    columnDefs: [
+                        {
+                            // For Responsive
+                            className: 'control',
+                            orderable: false,
+                            responsivePriority: 2,
+                            targets: 0
+                        },
+                        {
+                            targets: [1,-1],
+                            width: '3%',
+                            className: 'text-center p-1'
+                        },
+                        {
+                            targets: [1,2],
+                            width: '8%',
+                            className: 'text-center'
+                        },
+                        {
+                            targets: [3,4,5],
+                            width: '8%'
+                        },
+                        {
+                            targets: 6,
+                            width: '8%'
+                        },
+                        {
+                            targets: 9,
+                            width: '25%'
+                        }
+                    ],
+                    drawCallback: function () {
+                        $('[data-bs-toggle="tooltip"]').tooltip();
+                    }
+                });
+                const tblAksesorisInput = $('#tbl-material_filter input');
+                tblAksesorisInput.unbind();
+                tblAksesorisInput.bind('keyup', function (e) {
+                    if (e.keyCode === 13){
+                        aksesoris.search(this.value).draw();
+                    }
+                });
+            }
+
+            function edit(id){
+                Swal.fire({
+                    title: "{!! config('constants.CONFIRM_TITLE_EDIT') !!}",
+                    text: "{!! config('constants.WARNING_MESSAGE') !!}",
+                    icon: "question",
+                    showCancelButton: true,
+                    confirmButtonText: "Yes"
+                }).then(function (result) {
+                    if(result.value){
+                        url_redirect({
+                            url : baseUrl + '/master-rm/raw-material/'+id+'/edit',
+                            method  : "get"
+                        });
+                    }
+                });
+            }
+
+            function hapus(id){
+                let token=document.getElementsByName('_token');
+                Swal.fire({
+                    title: "{{config('constants.CONFIRM_TITLE_DELETE')}}",
+                    text: "{{config('constants.WARNING_MESSAGE')}}",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonText: "Yes, delete it!"
+                }).then((function (result) {
+                    if(result.value){
+                        url_redirect({
+                            url     : baseUrl + '/master-rm/raw-material/'+id,
+                            method  : "post",
+                            data    : {"_token" : token[0].value,"_method":'DELETE'}
+                        });
+                    }
+                }));
+            }
+            document.addEventListener('DOMContentLoaded',function (){
+                @if(Session::has('success'))
+                notifikasi('success','{{session('success')}}')
+                @endif
+                tableMaterial();
+            });
+        </script>
+    @endsection
+</x-layout>
