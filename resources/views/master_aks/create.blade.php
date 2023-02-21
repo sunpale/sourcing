f5a61586068e3239f121b784c03d763c7f135438
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-02-21 16:38:48 +0700

    Menambahkan attribute enctype untuk upload image dan action untuk save

--- resources/views/master_aks/create.blade.php
+++ resources/views/master_aks/create.blade.php
@@ -1,3 +1,3 @@
-<x-material-form :form="'AKS'" :group="$dataGroup->toArray()" :warna="$dataWarna->toArray()" :brand="$dataBrand->toArray()" :measure="$dataMeasure->toArray()" :warna-aks="$dataWarnaAks->toArray()">
+<x-material-form :form="'AKS'" :group="$dataGroup->toArray()" :warna="$dataWarna->toArray()" :brand="$dataBrand->toArray()" :measure="$dataMeasure->toArray()" :warna-aks="$dataWarnaAks->toArray()" action="{{route('aksesoris.store')}}" enctype="multipart/form-data">
 
 </x-material-form>
