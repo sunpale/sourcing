0d979e9dbe2c1c9f0b3f593366941bac5c329eed
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-05-10 18:54:17 +0700

    Update template velzon versi 2.4.0, dan update tampilan ke mode semibox

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -1,12 +1,11 @@
 <!doctype html>
-<html lang="en" data-layout="vertical" data-topbar="dark" data-sidebar="dark" data-layout-style="detached" data-layout-position="fixed" data-sidebar-size="lg" data-preloader="enable">
+<html lang="en" data-layout="semibox" data-sidebar-visibility="show" data-topbar="dark" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="enable" data-layout-position="fixed">
 <head>
     <meta charset="utf-8">
     <title>{{$title}} | Biensi Fesyenindo RM Manajemen</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Biensi Fesyenindo RM Manajemen" name="description"/>
-    <meta content="Biensi" name="author"/>
-
+    <meta content="Biensi IT Dept" name="author"/>
     <!-- App favicon -->
     <link rel="shortcut icon" href="{!! asset('src/images/favicon.ico') !!}">
 
@@ -19,7 +18,7 @@
     <!-- App Css-->
     <link href="{!! asset('src/css/app.min.css') !!}" rel="stylesheet" type="text/css"/>
     <!-- custom Css-->
-    <link href="{!! asset('src/css/custom.min.css') !!}" rel="stylesheet" type="text/css"/>
+    <link href="{!! asset('src/css/custom-semibox.min.css') !!}" rel="stylesheet" type="text/css"/>
 
     <!-- Component Css-->
     @if($datatable)
@@ -50,17 +49,18 @@
                 <div class="d-flex">
                     <!-- LOGO -->
                     <div class="navbar-brand-box horizontal-logo">
-                        <a href="/" class="logo logo-dark">
+                        <a href="index-2.html" class="logo logo-dark">
                             <span class="logo-sm">
-                                <img src="{!! asset('src/images/logo/logo-light.png') !!}" alt="" class="logo-height-sm">
+                                <img src="{!! asset('src/images/logo/logo-light.png') !!}" alt="" height="22">
                             </span>
                             <span class="logo-lg">
                                 <img src="{!! asset('src/images/logo/logo-sm-light.png') !!}" alt="" class="logo-height-lg">
                             </span>
                         </a>
-                        <a href="/" class="logo logo-light">
+
+                        <a href="index-2.html" class="logo logo-light">
                             <span class="logo-sm">
-                                <img src="{!! asset('src/images/logo/logo-light.png') !!}" alt="" class="logo-height-sm">
+                                <img src="{!! asset('src/images/logo/logo-light.png') !!}" alt="" height="22">
                             </span>
                             <span class="logo-lg">
                                 <img src="{!! asset('src/images/logo/logo-sm-light.png') !!}" alt="" class="logo-height-lg">
@@ -75,68 +75,37 @@
                         </span>
                     </button>
                 </div>
-                <div class="d-flex align-items-center">
-                    <div class="dropdown d-md-none topbar-head-dropdown header-item">
-                        <button type="button" class="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle" id="page-header-search-dropdown" data-bs-toggle="dropdown" aria-haspopup="true"
-                                aria-expanded="false">
-                            <i class="bx bx-search fs-22"></i>
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0"
-                             aria-labelledby="page-header-search-dropdown">
-                            <form class="p-3">
-                                <div class="form-group m-0">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control" placeholder="Search ..."
-                                               aria-label="Recipient's username">
-                                        <button class="btn btn-primary" type="submit"><i class="mdi mdi-magnify"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
 
+                <div class="d-flex align-items-center">
                     <div class="ms-1 header-item d-none d-sm-flex">
-                        <button type="button" class="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle"
-                                data-toggle="fullscreen">
+                        <button type="button" class="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle" data-toggle="fullscreen">
                             <i class='bx bx-fullscreen fs-22'></i>
                         </button>
                     </div>
 
                     <div class="ms-1 header-item d-none d-sm-flex">
-                        <button type="button"
-                                class="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle light-dark-mode">
+                        <button type="button" class="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle light-dark-mode">
                             <i class='bx bx-moon fs-22'></i>
                         </button>
                     </div>
-
                     <div class="dropdown ms-sm-3 header-item topbar-user">
                         <button type="button" class="btn" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <span class="d-flex align-items-center">
-                            <img class="rounded-circle header-profile-user" src="{!! asset('src/images/user-img.jpg') !!}" alt="Header Avatar">
+                            <img class="rounded-circle header-profile-user" src="{{asset('src/images/user-img.jpg')}}" alt="Header Avatar">
                             <span class="text-start ms-xl-2">
-                                <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{Auth::user()->name??'Guest'}}</span>
-                                <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">{{session()->get('role')??'Guest'}}</span>
+                                <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">Anna Adame</span>
+                                <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Founder</span>
                             </span>
                         </span>
                         </button>
                         <div class="dropdown-menu dropdown-menu-end">
                             <!-- item-->
-                            <h6 class="dropdown-header">Welcome {{Auth::user()->name??'Guest'}}</h6>
-                            <a class="dropdown-item" href="pages-profile.html"><i
-                                    class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i> <span
-                                    class="align-middle">Profile</span></a>
+                            <h6 class="dropdown-header">Welcome Anna!</h6>
+                            <a class="dropdown-item" href="#"><i class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Profile</span></a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="pages-profile-settings.html"><span
-                                    class="badge bg-soft-success text-success mt-1 float-end">New</span><i
-                                    class="mdi mdi-cog-outline text-muted fs-16 align-middle me-1"></i> <span
-                                    class="align-middle">Settings</span></a>
-                            <a class="dropdown-item" href="auth-lockscreen-basic.html"><i
-                                    class="mdi mdi-lock text-muted fs-16 align-middle me-1"></i> <span
-                                    class="align-middle">Lock screen</span></a>
-                            <a class="dropdown-item" href="#" id="btnlogout"><i
-                                    class="mdi mdi-logout text-muted fs-16 align-middle me-1"></i> <span
-                                    class="align-middle" data-key="t-logout">Logout</span></a>
+                            <a class="dropdown-item" href="#"><span class="badge bg-soft-success text-success mt-1 float-end">New</span><i class="mdi mdi-cog-outline text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Settings</span></a>
+                            <a class="dropdown-item" href="#"><i class="mdi mdi-lock text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Lock screen</span></a>
+                            <a class="dropdown-item" href="#" id="btnlogout"><i class="mdi mdi-logout text-muted fs-16 align-middle me-1"></i> <span class="align-middle" data-key="t-logout">Logout</span></a>
                         </div>
                     </div>
                 </div>
@@ -148,18 +117,18 @@
         <!-- LOGO -->
         <div class="navbar-brand-box">
             <!-- Dark Logo-->
-            <a href="/" class="logo logo-dark">
-                <span class="logo-sm">
-                    <img src="{!! asset('src/images/logo/logo-white.png') !!}" alt="" class="logo-height-sm">
+            <a href="index-2.html" class="logo logo-dark">
+               <span class="logo-sm">
+                    <img src="{!! asset('src/images/logo/logo-light.png') !!}" alt="" height="22">
                 </span>
                 <span class="logo-lg">
                     <img src="{!! asset('src/images/logo/logo-sm-light.png') !!}" alt="" class="logo-height-lg">
                 </span>
             </a>
             <!-- Light Logo-->
-            <a href="/" class="logo logo-light">
+            <a href="index-2.html" class="logo logo-light">
                 <span class="logo-sm">
-                    <img src="{!! asset('src/images/logo/logo-white.png') !!}" alt="" class="logo-height-sm">
+                    <img src="{!! asset('src/images/logo/logo-light.png') !!}" alt="" class="logo-height-sm">
                 </span>
                 <span class="logo-lg">
                     <img src="{!! asset('src/images/logo/logo-sm-light.png') !!}" alt="" class="logo-height-lg">
@@ -169,6 +138,7 @@
                 <i class="ri-record-circle-line"></i>
             </button>
         </div>
+
         <div id="scrollbar">
             <div class="container-fluid">
                 <div id="two-column-menu">
@@ -292,51 +262,56 @@
             </div>
             <!-- Sidebar -->
         </div>
-        <div class="sidebar-background"></div>
     </div>
-    <!-- ========== End App Menu ========== -->
-
     <!-- Left Sidebar End -->
     <!-- Vertical Overlay-->
     <div class="vertical-overlay"></div>
 
-    <!-- Start main Content here -->
+    <!-- ============================================================== -->
+    <!-- Start right Content here -->
+    <!-- ============================================================== -->
     <div class="main-content">
         <div class="page-content">
             <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12">
-                        {{--Start Page Title--}}
-                        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                            {!! \Diglactic\Breadcrumbs\Breadcrumbs::render($breadcrumbs) !!}
-                        </div>
-                        {{--End Page Title--}}
+                <div class="mb-0">
+                    <div class="row">
+                        <div class="col-12">
+                            {{--Start Page Title--}}
+                            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                                {!! \Diglactic\Breadcrumbs\Breadcrumbs::render($breadcrumbs) !!}
+                            </div>
+                            {{--End Page Title--}}
+                        </div> <!-- end col -->
                     </div>
+                    {{$slot}}
                 </div>
-                {{$slot}}
             </div>
             <!-- container-fluid -->
         </div>
-    </div>
-    <!-- End main Content here -->
-    <footer class="footer">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-6">
-                    {{date('Y')??''}} &copy; Digital Compliance by <a href="#" title="Muhammad Ihsan Nur Hikam">IT Development Biensi</a>.
-                </div>
-                <div class="col-sm-6">
+        <!-- End Page-content -->
+        <footer class="footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-6">
+                        {{date('Y')??''}} &copy; Digital Compliance by <a href="#" title="Muhammad Ihsan Nur Hikam">IT Development Biensi</a>.
+                    </div>
+                    <div class="col-sm-6">
+                    </div>
                 </div>
             </div>
-        </div>
-    </footer>
+        </footer>
+    </div>
+    <!-- end main content-->
+
 </div>
-<!-- End Begin Page -->
+<!-- END layout-wrapper -->
+
 <!--start back-to-top-->
 <button onclick="topFunction()" class="btn btn-danger btn-icon" id="back-to-top">
     <i class="ri-arrow-up-line"></i>
 </button>
 <!--end back-to-top-->
+
 <!--preloader-->
 <div id="preloader">
     <div id="status">
@@ -355,59 +330,61 @@
 <script src="{!! asset('src/js/app.js') !!}"></script>
 <script src="{!! asset('src/js/me.js') !!}"></script>
 <script>const baseUrl = window.location.origin;</script>
-
 <!-- Component JAVASCRIPT -->
 @if($jquery)
-    <script src="{!! asset('src/libs/jquery/jquery.min.js') !!}"></script>
+<script src="{!! asset('src/libs/jquery/jquery.min.js') !!}"></script>
 @endif
 @if($datatable)
-    <script src="{!! asset('src/libs/datatables/js/jquery.dataTables.min.js') !!}"></script>
-    <script src="{!! asset('src/libs/datatables/js/dataTables.bootstrap5.min.js') !!}"></script>
-    <script src="{!! asset('src/libs/datatables/js/dataTables.responsive.min.js') !!}"></script>
-    <script src="{!! asset('src/libs/datatables/js/responsive.bootstrap5.min.js') !!}"></script>
-    <script src="{!! asset('src/libs/datatables/js/config.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/jquery.dataTables.min.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/dataTables.bootstrap5.min.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/dataTables.responsive.min.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/responsive.bootstrap5.min.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/config.js') !!}"></script>
 @endif
 @if($datatableButton)
-    <script src="{!! asset('src/libs/datatables/js/button/dataTables.buttons.min.js') !!}"></script>
-    <script src="{!! asset('src/libs/datatables/js/button/buttons.bootstrap5.min.js') !!}"></script>
-    <script src="{!! asset('src/libs/datatables/js/button/jszip.min.js') !!}"></script>
-    <script src="{!! asset('src/libs/datatables/js/button/pdfmake.min.js') !!}"></script>
-    <script src="{!! asset('src/libs/datatables/js/button/vfs_fonts.js') !!}"></script>
-    <script src="{!! asset('src/libs/datatables/js/button/buttons.html5.min.js') !!}"></script>
-    <script src="{!! asset('src/libs/datatables/js/button/buttons.print.min.js') !!}"></script>
-    <script src="{!! asset('src/libs/datatables/js/button/buttons.colVis.min.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/button/dataTables.buttons.min.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/button/buttons.bootstrap5.min.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/button/jszip.min.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/button/pdfmake.min.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/button/vfs_fonts.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/button/buttons.html5.min.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/button/buttons.print.min.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/button/buttons.colVis.min.js') !!}"></script>
 @endif
 @if($datatableSelect)
-    <script src="{!! asset('src/libs/datatables/js/select/dataTables.select.min.js') !!}"></script>
-    <script src="{!! asset('src/libs/datatables/js/select/select.bootstrap5.min.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/select/dataTables.select.min.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/select/select.bootstrap5.min.js') !!}"></script>
 @endif
 @if($datatableRowGroup)
-    <script src="{!! asset('src/libs/datatables/js/rowgroup/dataTables.rowGroup.min.js') !!}"></script>
+<script src="{!! asset('src/libs/datatables/js/rowgroup/dataTables.rowGroup.min.js') !!}"></script>
 @endif
 @if($toastify)
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 @endif
 @if($sweetalert)
-    <script src="{!! asset('src/libs/sweetalert2/sweetalert2.min.js') !!}"></script>
+<script src="{!! asset('src/libs/sweetalert2/sweetalert2.min.js') !!}"></script>
 @endif
 @if($freezeUi)
-    <script src="{!! asset('src/libs/freeze-ui/freeze-ui.min.js') !!}"></script>
+<script src="{!! asset('src/libs/freeze-ui/freeze-ui.min.js') !!}"></script>
 @endif
 @if($select2)
-    <script src="{!! asset('src/libs/select2/select2.min.js') !!}"></script>
+<script src="{!! asset('src/libs/select2/select2.min.js') !!}"></script>
 @endif
 @if($flatpickr)
-    <script src="{!! asset('src/libs/flatpickr/flatpickr.min.js') !!}"></script>
+<script src="{!! asset('src/libs/flatpickr/flatpickr.min.js') !!}"></script>
 @endif
 @if($dropzone)
-    <script src="{!! asset('src/libs/dropzone/dropzone-min.js') !!}"></script>
+<script src="{!! asset('src/libs/dropzone/dropzone-min.js') !!}"></script>
 @endif
 @if($cleavejs)
-    <script src="{!! asset('src/libs/cleave.js/cleave.min.js') !!}"></script>
-    <script src="{!! asset('src/libs/cleave.js/cleave-phone.id.js') !!}"></script>
+<script src="{!! asset('src/libs/cleave.js/cleave.min.js') !!}"></script>
+<script src="{!! asset('src/libs/cleave.js/cleave-phone.id.js') !!}"></script>
 @endif
 @if($glightbox)
-    <script src="{!! asset('src/libs/glightbox/js/glightbox.min.js') !!}"></script>
+<script src="{!! asset('src/libs/glightbox/js/glightbox.min.js') !!}"></script>
+@endif
+@if($jsvalidation)
+<script src="{!! asset('vendor/jsvalidation/js/jsvalidation.min.js') !!}"></script>
 @endif
 @yield('script')
 <script>
