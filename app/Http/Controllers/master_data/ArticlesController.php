fb24f93ac1f6819c3d10ec6588dca9d74588eaa7
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-05-10 18:38:35 +0700

    Perbaikan Bug url redirect dan juga menambahkan method baru
    Method yang ditambahkan adalah untuk mengambil data artikel untuk tampil di komponen dropdown select2 dan pencarian informasi artikel berdasarkan ID

--- app/Http/Controllers/master_data/ArticlesController.php
+++ app/Http/Controllers/master_data/ArticlesController.php
@@ -10,6 +10,7 @@ use App\Models\master_data\Brand;
 use App\Models\master_warna\Pantone;
 use App\Services\ImageManipulation\ImageManipulationService;
 use Exception;
+use Illuminate\Http\Request;
 use Spatie\MediaLibrary\MediaCollections\Exceptions\FileDoesNotExist;
 use Spatie\MediaLibrary\MediaCollections\Exceptions\FileIsTooBig;
 use Yajra\DataTables\Facades\DataTables;
@@ -47,7 +48,7 @@ class ArticlesController extends Controller
             $filename = $request->kode.'.'.$image->extension();
             $article->addMediaFromRequest('img_file')->usingName($request->kode)->usingFileName($filename)->storingConversionsOnDisk('media-thumb')->toMediaCollection('articles');
         }
-        return redirect()->route('bom.articles.index')->with('success',config('constants.SUCCESS_SAVE'));
+        return redirect()->route('articles.index')->with('success',config('constants.SUCCESS_SAVE'));
     }
 
     /**
@@ -67,7 +68,7 @@ class ArticlesController extends Controller
                     return '<div class="dropdown d-inline-block"><button class="btn btn-soft-primary btn-sm dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="ri-equalizer-fill align-middle"></i></button><ul class="dropdown-menu dropdown-menu-end"><li><a id="btnedit" href="#" data-bs-toggle="tooltip" data-placement="auto" title="Edit Data" class="dropdown-item" onclick=edit("'.$row->id.'")><i class="ri-edit-fill"></i> Edit Data</a></li><li><a id="btnhapus" href="#" data-bs-toggle="tooltip" data-placement="auto" title="Hapus Data" class="dropdown-item" onclick=hapus("'.$row->id.'")><i class="ri-close-circle-fill"></i> Hapus Data</a></li></ul></div>';
                 })->rawColumns(['image','action'])->make();
         }
-        return redirect()->route('bom.articles.index');
+        return redirect()->route('articles.index');
     }
 
     public function edit(Article $article)
@@ -75,7 +76,7 @@ class ArticlesController extends Controller
         $brand = Brand::where('kode','!=',99)->pluck('brand','id');
         $pantone = Pantone::all()->pluck('full_pantone','id');
         $editMode = true;
-        return view('bom.article.create',compact('brand','pantone','article','editMode'));
+        return view('master_data.article.create',compact('brand','pantone','article','editMode'));
     }
 
     /**
@@ -91,7 +92,7 @@ class ArticlesController extends Controller
             $article->media()->delete();
             $article->addMediaFromRequest('img_file')->usingName($request->kode)->usingFileName($filename)->storingConversionsOnDisk('media-thumb')->toMediaCollection('articles');
         }
-        return redirect()->route('bom.articles.index')->with('success',config('constants.SUCCESS_UPDATE'));
+        return redirect()->route('articles.index')->with('success',config('constants.SUCCESS_UPDATE'));
     }
 
     public function destroy(Article $article)
@@ -102,6 +103,48 @@ class ArticlesController extends Controller
             CustomMedia::where('model_id',$article->id)->update(['collection_name' => 'media-archived','disk' => 'media-archived','conversions_disk' => 'media-archived']);
             $this->imageManipulation->move_image($image,true);
         }
-        return redirect()->route('bom.articles.index')->with('success',config('constants.SUCCESS_DELETE'));
+        return redirect()->route('articles.index')->with('success',config('constants.SUCCESS_DELETE'));
+    }
+
+    public function getArticles(Request $request){
+        /*get data pencarian dan jumlah halaman dari komponen select2*/
+        $search = $request->search;
+        $halaman = $request->page;
+        $searchData = empty($search) ? "" : $search;
+        /*$type = $request->type;*/
+        /*end*/
+        /*Set jumlah data per halaman*/
+        $pageLoad = 25;
+        /*End*/
+        /*Cek jumlah halaman, kemudian dikurang satu (offset data di mulai dari 0)*/
+        if($halaman==1){
+            $page=0;
+        }else{
+            /*Jika halaman lebih dari 1, maka setelah dikurang satu dikalikan jumlah data per halaman untuk mendapatkan data offset halaman berikutnya*/
+            $page=($halaman-1)*$pageLoad;
+            /*end*/
+        }
+        /*end*/
+        /*Memanggil data dan jumlah data dari database*/
+        $dataItem=Article::select(['id','kode','name'])->where('kode','LIKE','%'.$searchData.'%')->limit($pageLoad)->offset($page)->get();
+        $dataCount=Article::select(['id','kode','name'])->where('kode','LIKE','%'.$searchData.'%')->count();
+        /*End*/
+
+        /*Mengubah hasil data dari database sesuai dengan format dari select2*/
+        $result=array();
+        foreach ($dataItem as $item){
+            $result[] = array("id" => $item->id,"text"=>$item->kode.' - '.$item->name);
+        }
+        $resultQuery['items']=$result;
+        $resultQuery['total_count']=$dataCount;
+        /*End*/
+        return response()->json($resultQuery);
+    }
+
+    public function findArticle(Article $article){
+        $result = $article->with(['pantone:id,kode,pantone','brand:id,brand'])->select(['kode','modul','name','pantone_id','brand_id','designer'])
+            ->where('id',$article->id)->get();
+        $image = $article->getFirstMediaUrl('articles');
+        return response()->json(compact('result','image'));
     }
 }
