37df0a6c7712c3a978407fa4c920ed3ec60c6b15
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-05-11 22:52:15 +0700

    Implementasi integrasi media library pada method store dan update
    - Optimalisasi pemanggilan data pada method create dan edit
    - Menghapus method save dan change yang sudah tidak digunakan

--- app/Http/Controllers/master_rm/MaterialsController.php
+++ app/Http/Controllers/master_rm/MaterialsController.php
@@ -12,6 +12,8 @@ use App\Models\master_rm\Material;
 use App\Models\master_warna\Color;
 use App\Models\master_warna\Pantone;
 use Illuminate\Http\Request;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\FileDoesNotExist;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\FileIsTooBig;
 use Yajra\DataTables\Facades\DataTables;
 
 class MaterialsController extends Controller
@@ -29,71 +31,62 @@ class MaterialsController extends Controller
         $pantone = Pantone::all()->pluck('full_pantone','id');
         $komposisi = Komposisi::select(['id','komposisi'])->pluck('komposisi','id');
         $measure = Measure::select(['id','kode'])->pluck('kode','id');
-        $data = [
-            'dataFabric' => $fabric,
-            'dataWarna' => $warna,
-            'dataBrand' => $brand,
-            'dataPantone' => $pantone,
-            'dataKomposisi' => $komposisi,
-            'dataMeasure' => $measure
-        ];
-        return view('master_rm.create',$data);
+        return view('master_rm.create',compact('fabric','warna','brand','pantone','komposisi','measure'));
     }
 
+    /**
+     * @throws FileDoesNotExist
+     * @throws FileIsTooBig
+     */
     public function store(MaterialRequest $request)
     {
-        $data = $request->except(['_token','number']);
+        $data = $request->except(['_token','number','img_file']);
         $kodeGenerated = $request->number;
         $prefix = substr($kodeGenerated,0,strlen($kodeGenerated)-4);
         $kode = $this->createCode($prefix);
         $data['kode'] = $kode;
-        $imageSource = $request->file('img_file');
+        $data['unit_price'] = str_replace('.','',$request->unit_price);
         if ($request->hasFile('img_file') && $request->file('img_file')->isValid()){
-            $path = 'private/rm/';
-            $data['image_path'] = $path;
-            $data['image_name'] = $kode.'.'.$imageSource->extension();
-
-            if($this->save($data)){
-                $imageSource->storeAs($path,$kode.'.'.$imageSource->extension());
-            }
+            $image = $request->file('img_file');
+            $filename = $kode.'.'.$image->extension();
+            $material = Material::create($data);
+            $material->addMediaFromRequest('img_file')->usingName($kode)->usingFileName($filename)->storingConversionsOnDisk('media-thumb')->toMediaCollection('raw material');
         }else{
-            $this->save($data);
+            Material::create($data);
         }
-        return redirect()->route('master-rm.raw-material.index')->with('success',config('constants.SUCCESS_SAVE'));
+        return redirect()->route('master-rm.raw-material.index')->with(config('constants.SUCCESS_SAVE'));
     }
 
     public function show(Material $raw_material)
     {
-        $data = ['rm'=>$raw_material->with(['user:id,username','fabric:id,description','color:id,description','brand:id,brand','supplier:id,name','komposisi:id,komposisi','measure:id,kode,measure_name'])->where('kode',$raw_material->kode)->get()];
-        return view('master_rm.detail',$data);
+        $rm = $raw_material->with(['user:id,username','fabric:id,description','color:id,description','brand:id,brand','supplier:id,name','komposisi:id,komposisi','measure:id,kode,measure_name'])->where('kode',$raw_material->kode)->get();
+        $image = $rm[0]->getFirstMediaUrl('raw material');
+        return view('master_rm.detail',compact('rm','image'));
     }
 
     public function data(){
         if(request()->ajax()){
-            $query = Material::select(['kode','kode_infor','fabric_id','color_id','brand_id','supplier_id','komposisi_id','item_name','item_desc','measure_id'])
+            $query = Material::select(['id','kode','kode_infor','fabric_id','color_id','brand_id','supplier_id','komposisi_id','item_name','item_desc','measure_id'])
                 ->with(['fabric:id,description','color:id,description','brand:id,brand','supplier:id,name','komposisi:id,komposisi','measure:id,kode,measure_name'])->where('fabric_id','<>',null);
             return DataTables::eloquent($query)->order(function ($query){$query->orderBy('created_at','asc');})
                 ->addIndexColumn()->addColumn('responsive',function (){return '';})
                 ->addColumn('action',function ($row){
-                    return '<div class="dropdown d-inline-block"><button class="btn btn-soft-primary btn-sm dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="ri-equalizer-fill align-middle"></i></button><ul class="dropdown-menu dropdown-menu-end"><li><a href="#" data-bs-toggle="tooltip" data-placement="auto" title="Edit Data" class="dropdown-item" onclick=edit("'.$row->kode.'")><i class="ri-edit-fill"></i> Edit Data</a></li><li><a href="#" data-bs-toggle="tooltip" data-placement="auto" title="Hapus Data" class="dropdown-item" onclick=hapus("'.$row->kode.'")><i class="ri-close-circle-fill"></i> Hapus Data</a></li><li><a href="#" data-bs-toggle="tooltip" data-placement="auto" title="View Detail" class="dropdown-item" onclick=view("'.$row->kode.'")><i class="ri-file-search-line"></i> View Detail</a></li></ul></div>';
+                    return '<div class="dropdown d-inline-block"><button class="btn btn-soft-primary btn-sm dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="ri-equalizer-fill align-middle"></i></button><ul class="dropdown-menu dropdown-menu-end"><li><a href="#" data-bs-toggle="tooltip" data-placement="auto" title="Edit Data" class="dropdown-item" onclick=edit("'.$row->id.'")><i class="ri-edit-fill"></i> Edit Data</a></li><li><a href="#" data-bs-toggle="tooltip" data-placement="auto" title="Hapus Data" class="dropdown-item" onclick=hapus("'.$row->id.'")><i class="ri-close-circle-fill"></i> Hapus Data</a></li><li><a href="#" data-bs-toggle="tooltip" data-placement="auto" title="View Detail" class="dropdown-item" onclick=view("'.$row->id.'")><i class="ri-file-search-line"></i> View Detail</a></li></ul></div>';
                 })->make(true);
         }
         return redirect()->route('master-rm.raw-material.index');
     }
 
-
     public function edit(Material $raw_material)
     {
-        $data = [
-            'dataFabric' => Fabric::all()->pluck('full_description','id'),
-            'dataWarna' => Color::all()->pluck('full_description','id'),
-            'dataBrand' => Brand::all()->pluck('full_brand','id'),
-            'dataPantone' => Pantone::all()->pluck('full_pantone','id'),
-            'dataKomposisi' => Komposisi::select(['id','komposisi'])->pluck('komposisi','id'),
-            'dataMeasure' => Measure::select(['id','kode'])->pluck('kode','id'),
-            'dataRm'    => $raw_material->with('supplier:id,kode,name')->where('kode',$raw_material->kode)->get()
-        ];
-        return view('master_rm.edit',$data);
+        $fabric = Fabric::all()->pluck('full_description','id');
+        $warna = Color::all()->pluck('full_description','id');
+        $brand = Brand::all()->pluck('full_brand','id');
+        $pantone = Pantone::all()->pluck('full_pantone','id');
+        $komposisi = Komposisi::select(['id','komposisi'])->pluck('komposisi','id');
+        $measure = Measure::select(['id','kode'])->pluck('kode','id');
+        $material = $raw_material->with('supplier:id,kode,name')->where('id',$raw_material->id)->get();
+        return view('master_rm.edit',compact('fabric','warna','brand','pantone','komposisi','measure','material'));
     }
 
     public function update(MaterialRequest $request, Material $raw_material)
@@ -104,38 +97,29 @@ class MaterialsController extends Controller
         $prefix = substr($kodeGenerated,0,strlen($kodeGenerated)-4);
         $kode = strcmp($prefix,$prefixOldKode) == 0 ? $raw_material->kode : $this->createCode($prefix);
         $data['kode'] = $kode;
-        $imageSource = $request->file('img_file');
-        if ($request->hasFile('img_file') && $request->file('img_file')->isValid()){
-            $path = 'private/rm';
-            $data['image_path'] = $path;
-            $data['image_name'] = $kode.'.'.$imageSource->extension();
+        $data['unit_price'] = str_replace('.','',$request->unit_price);
 
-            if ($this->change($data,$raw_material->kode)){
-                $imageSource->storeAs($path,$kode.'.'.$imageSource->extension());
-            }
+        if ($request->hasFile('img_file') && $request->file('img_file')->isValid()){
+            $image = $request->file('img_file');
+            $filename = $kode.'.'.$image->extension();
+            Material::where('id',$raw_material->id)->update($data);
+            $raw_material->media()->delete();
+            $raw_material->addMediaFromRequest('img_file')->usingName($kode)->usingFileName($filename)->storingConversionsOnDisk('media-thumb')->toMediaCollection('raw material');
         }else{
-            $this->change($data,$raw_material->kode);
+            Material::where('id',$raw_material->id)->update($data);
         }
         return redirect()->route('master-rm.raw-material.index')->with('success',config('constants.SUCCESS_UPDATE'));
     }
 
     public function destroy(Material $raw_material)
     {
-        Material::destroy($raw_material->kode);
+        Material::destroy($raw_material->id);
         return redirect()->route('master-rm.raw-material.index')->with('success',config('constants.SUCCESS_DELETE'));
     }
 
     public function viewImage($filename){
         $file = storage_path('app/private/rm/'.$filename);
         return response()->file($file);
-    }
-
-    private function save(array $data){
-        return Material::create($data);
-    }
-
-    private function change(array $data,string $kode){
-        return Material::where('kode',$kode)->update($data);
     }
 
     private function createCode(string $prefix){
