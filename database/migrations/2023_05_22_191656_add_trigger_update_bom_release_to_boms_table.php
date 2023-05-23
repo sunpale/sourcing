ef4d54730babe1c64edf3fe078fe69b00ff925cd
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-05-23 13:14:49 +0700

    Membuat trigger pada tabel bom untuk update status bom_release pada tabel artikel

---
+++ database/migrations/2023_05_22_191656_add_trigger_update_bom_release_to_boms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        if (env('DB_CONNECTION')==='sqlsrv') {
+            DB::unprepared('CREATE TRIGGER [dbo].[update_bom_status_on_article]
+ON [dbo].[boms]
+FOR INSERT
+AS
+BEGIN
+    DECLARE @articleId BIGINT;
+		SELECT @articleId = article_id FROM INSERTED boms;
+		UPDATE articles SET bom_release=1 WHERE id = @articleId;
+END');
+        }
+        elseif(env('DB_CONNECTION')==='mysql'){
+            DB::unprepared('CREATE TRIGGER `update_bom_status_on_article` AFTER INSERT ON `boms` FOR EACH ROW BEGIN
+	UPDATE articles SET bom_release=1 WHERE id = new.article_id;
+END;');
+        }
+
+    }
+
+    public function down(): void
+    {
+        if (env('DB_CONNECTION')==='sqlsrv'){
+            DB::unprepared('DROP TRIGGER IF EXISTS update_bom_status_on_article;');
+        }
+        elseif(env('DB_CONNECTION')==='mysql'){
+            DB::unprepared('DROP TRIGGER IF EXISTS update_bom_status_on_article');
+        }
+    }
+};
