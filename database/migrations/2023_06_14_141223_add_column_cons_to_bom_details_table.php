468e75ab0edfad94f5bb94922cad8383c76c3c6b
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-06-14 16:35:08 +0700

    mengubah tipe data kolom conse menjadi decimal

---
+++ database/migrations/2023_06_14_141223_add_column_cons_to_bom_details_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Schemas\Grammars\CustomSqlServerGrammar;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        if (env('DB_CONNECTION')==='sqlsrv') {
+            DB::connection()->setSchemaGrammar(new CustomSqlServerGrammar());
+        }
+        Schema::table('bom_details', function (Blueprint $table) {
+            $table->decimal('cons',8,4)->default(0);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('bom_details', function (Blueprint $table) {
+            $table->dropColumn('cons');
+        });
+    }
+};
