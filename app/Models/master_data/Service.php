8a5a7e3c41045386696bf9931d50cd95ad4a72ee
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-06-14 16:34:06 +0700

    Menghubungkan model service dengan BomDetailService

--- app/Models/master_data/Service.php
+++ app/Models/master_data/Service.php
@@ -2,9 +2,11 @@
 
 namespace App\Models\master_data;
 
+use App\Models\BOM\Bom_detail_service;
 use App\Traits\CustomSoftDelete;
 use App\Traits\UserInput;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Service extends Model
@@ -12,4 +14,9 @@ class Service extends Model
     use UserInput,CustomSoftDelete,SoftDeletes{CustomSoftDelete::runSoftDelete insteadof SoftDeletes;}
 
     protected $fillable = ['name','remarks'];
+
+    public function BomDetailService(): HasMany
+    {
+        return $this->hasMany(Bom_detail_service::class);
+    }
 }
