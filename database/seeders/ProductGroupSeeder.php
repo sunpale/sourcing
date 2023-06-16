0b4baffececd11e462e5d1bd360dc4644c61079f
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-06-16 10:51:52 +0700

    Memperbaikai ProductGroupSeeder dengan menambahkan data type

--- database/seeders/ProductGroupSeeder.php
+++ database/seeders/ProductGroupSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\master_aks\ProductGroup;
+use App\Models\master_material\ProductGroup;
 use Illuminate\Database\Seeder;
 
 class ProductGroupSeeder extends Seeder
@@ -10,15 +10,8 @@ class ProductGroupSeeder extends Seeder
     public function run(): void
     {
         ProductGroup::insert([
-            'kode'          => 'NG',
-            'group'         => 'Non Group',
-            'created_by'    => 1,
-            'updated_by'    => 1,
-            'created_at'    => now(),
-            'updated_at'    => now()],
-        );
-        ProductGroup::insert([
             'kode'          => 'BD',
+            'type'          => 'Raw Material',
             'group'         => 'Body',
             'created_by'    => 1,
             'updated_by'    => 1,
@@ -28,6 +21,7 @@ class ProductGroupSeeder extends Seeder
 
         ProductGroup::insert([
             'kode'          => 'RB',
+            'type'          => 'Raw Material',
             'group'         => 'Rib',
             'created_by'    => 1,
             'updated_by'    => 1,
