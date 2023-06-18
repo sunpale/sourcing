771d721f9524e29d97467dce9b35ea89567cec58
Muhammad Ihsan <nh.ihsan@gmail.com>
2023-06-18 22:52:56 +0700

    Merelasikan model supplier dengan product group

--- app/Models/master_material/ProductGroup.php
+++ app/Models/master_material/ProductGroup.php
@@ -2,9 +2,11 @@
 
 namespace App\Models\master_material;
 
+use App\Models\master_data\Supplier;
 use App\Traits\CustomSoftDelete;
 use App\Traits\UserInput;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductGroup extends Model
@@ -16,5 +18,10 @@ class ProductGroup extends Model
     public function getFullGroupAttribute(): string
     {
         return $this->kode.' - '.$this->group;
+    }
+
+    public function Supplier(): HasMany
+    {
+        return $this->hasMany(Supplier::class,'product_group_id','id');
     }
 }
